d30a5f52acf5e9d648ee9b13aa02e84eebf4c58e
stevehadeler <115306779+stevehadeler@users.noreply.github.com>
2023-03-28 15:31:51 -0500

    Update streamlit_app.py
    Committing pandas and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 #streamlit app python file
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -8,3 +9,5 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard Boiled Free-Range Egg")
 streamlit.text("🥑 🍞 Avacado Toast")
 streamlit.header("🍌 🍊 Build Your Own Fruit Smoothie 🥝 🍇")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list);
